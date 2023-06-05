7d3bba7d4970764d9d821e75d0f543bc4ffc100f
Iswan Jumat <info@iswanjumat.com>
2023-06-05 22:07:32 +0800

    refactor

--- generateW.js
+++ generateW.js
@@ -5,6 +5,7 @@ const directoryPath = "files/worker_thread";
 export const generateW = (header, filePath, fileSizeInBytes) => {
   return new Promise((resolve, reject) => {
     const start = Date.now();
+    // start worker
     const worker = new Worker("./worker.js", {
       workerData: {
         header,
@@ -14,6 +15,7 @@ export const generateW = (header, filePath, fileSizeInBytes) => {
       },
     });
 
+    // listen for worker message
     worker.once("message", (data) => {
       console.log(`worker [${worker.threadId}]: generate file done in [${Date.now() - start}ms]`);
       resolve("done");
